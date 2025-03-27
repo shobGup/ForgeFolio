3c74b65e75510580086d2d373303a70a56e5ea9d
dangcello <andrewdang0901@gmail.com>
2025-03-27 10:49:00 -0500

    css for se all works

--- src/components/SeeAll/SeeAllHeader.js
+++ src/components/SeeAll/SeeAllHeader.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import './SeeAllHeader.css';
 
-const SeeAllHeader = ({ work }) => {
+const SeeAllHeader = () => {
     return (
-        <div className="header-container">
-            <button className="back-button">Back</button>
-            <input className="search-bar" type="text" placeholder="Search..." />
-            <select className="sort-dropdown">
-                <option value="default">Sort</option>
-                <option value="name">Name</option>
-                <option value="date">Date</option>
-                <option value="tags">Tags</option>
-            </select>
+        <div style={{ position: 'relative' }}>
+            <div className="header-background"></div>
+            <div className="header-container">
+                <button className="back-button">Back</button>
+                <div className="search-wrapper">
+                    <img 
+                        src={process.env.PUBLIC_URL + "/images/search.svg"} 
+                        alt="Search Icon" 
+                        className="search-icon"
+                    />
+                    <input className="search-bar" type="text" placeholder="Search by name" />
+                </div>
+                <div className="dropdown-wrapper">
+                    <select className="sort-dropdown">
+                        <option value="default">Sort</option>
+                        <option value="name">Name</option>
+                        <option value="date">Date</option>
+                        <option value="tags">Tags</option>
+                    </select>
+                    <img 
+                        src={process.env.PUBLIC_URL + "/images/sort_icon.png"} 
+                        alt="Sort Icon" 
+                        className="sort-icon"
+                    />
+                    <img 
+                        src={process.env.PUBLIC_URL + "/images/down_icon.png"} 
+                        alt="Dropdown Arrow" 
+                        className="dropdown-arrow"
+                    />
+                </div>
+            </div>
         </div>
     );
 };
