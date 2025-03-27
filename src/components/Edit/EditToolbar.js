410bfa30387d4d29e5cf705803ed33b0873dcf66
shobGup <shobu02@gmail.com>
2025-03-26 19:05:10 -0500

    Cleaning up

--- src/components/Edit/EditToolbar.js
+++ src/components/Edit/EditToolbar.js
@@ -5,35 +5,31 @@ import './styles/EditToolbar.css';
 const EditToolbar = () => {
     return (
         <div className="edit-toolbar">
-            <div className="buttons">
-                <button className="item button delete-button">
-                    Delete
-                </button>
-                <button className="item button help-button">
-                    Help
-                </button>
-            </div>
-            <div className="icons">
-                <button className="item icon print-button">
-                    <img src={process.env.PUBLIC_URL + "/images/toolbar_icons/printer_icon.png"} className="icon print-icon" alt="print"/>
-                </button>
-                <button className="item icon undo-button">
-                    <img src={process.env.PUBLIC_URL + "/images/toolbar_icons/undo_icon.png"} className="icon undo-icon" alt="undo"/>
-                </button>
-                <button className="item icon redo-button">
-                    <img src={process.env.PUBLIC_URL + "/images/toolbar_icons/redo_icon.png"} className="icon redo-icon" alt="redo"/>
-                </button>
-                <button className="item icon select-button">
-                    <img src={process.env.PUBLIC_URL + "/images/toolbar_icons/cursor_icon.png"} className="icon select-icon" alt="select"/>
-                </button>
-                <button className="item icon textbox-button">
-                    <img src={process.env.PUBLIC_URL + "/images/toolbar_icons/textbox_icon.png"} className="icon textbox-icon" alt="add textbox"/>
-                </button>
-                <button className="item icon image-button">
-                    {/* eslint-disable-next-line jsx-a11y/img-redundant-alt*/}
-                    <img src={process.env.PUBLIC_URL + "/images/toolbar_icons/img_icon.svg"} className="icon image-icon" alt="add image"/>
-                </button>
-            </div>
+            <button className="item button delete-button">
+                Delete
+            </button>
+            <button className="item button help-button">
+                Help
+            </button>
+            <button className="item icon print-button">
+                <img src={process.env.PUBLIC_URL + "/images/toolbar_icons/printer_icon.png"} className="icon print-icon" alt="print"/>
+            </button>
+            <button className="item icon undo-button">
+                <img src={process.env.PUBLIC_URL + "/images/toolbar_icons/undo_icon.png"} className="icon undo-icon" alt="undo"/>
+            </button>
+            <button className="item icon redo-button">
+                <img src={process.env.PUBLIC_URL + "/images/toolbar_icons/redo_icon.png"} className="icon redo-icon" alt="redo"/>
+            </button>
+            <button className="item icon select-button">
+                <img src={process.env.PUBLIC_URL + "/images/toolbar_icons/cursor_icon.png"} className="icon select-icon" alt="select"/>
+            </button>
+            <button className="item icon textbox-button">
+                <img src={process.env.PUBLIC_URL + "/images/toolbar_icons/textbox_icon.png"} className="icon textbox-icon" alt="add textbox"/>
+            </button>
+            <button className="item icon image-button">
+                {/* eslint-disable-next-line jsx-a11y/img-redundant-alt*/}
+                <img src={process.env.PUBLIC_URL + "/images/toolbar_icons/img_icon.svg"} className="icon image-icon" alt="add image"/>
+            </button>
             <div className='vertical-line'/>
             <button className="item button background-button">
                 Background
