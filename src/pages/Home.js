976924d0e737eec076527046badf91747bac2d49
dangcello <andrewdang0901@gmail.com>
2025-04-01 10:26:16 -0500

    added extra protfolio for testing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,6 +26,7 @@ const Home = () => {
     const [allPortfolios, setAllPortfolios] = React.useState([
         { title: 'Dreamworks', imageUrl: '/images/portfolio2.png', createdDate: '03/01/2025', description: 'A portfolio for dreamworks.', tags: ['art', 'paintings', 'cartoon'], link : 30},
         { title: 'Cellos at Texas', imageUrl: '/images/portfolio1.png', createdDate: '02/25/2025', description: 'A portfolio for cellos at texas.', tags: ['audio', 'expressionism'], link : 7},
+        { title: 'Impressionism', imageUrl: '/images/monet_port.png', createdDate: '02/25/2024', description: 'A portfolio for impressionism.', tags: ['art', 'impressionism'], link : 10},
     ]);
 
     const recentPortfolios = allPortfolios
